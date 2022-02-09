235b84e5ccabaa1295d0817920973391b0851ec3
femke <femke@elgentos.nl>
2022-02-09 16:43:14 +0100

    composer.json autoloads InstallPlugin to copy symlink and cypress.json

--- src/Composer/InstallPlugin.php
+++ src/Composer/InstallPlugin.php
@@ -6,21 +6,51 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-class InstallPlugin implements PluginInterface
+final class InstallPlugin implements PluginInterface
 {
 
-    public function activate(Composer $composer, IOInterface $io)
+    /** @noinspection RealpathInStreamContextInspection */
+    public function activate(Composer $composer, IOInterface $io): void
     {
-        var_dump('HeyyyyACTIVATE');
+        $projectPath = realpath($composer->getConfig()->get('vendor-dir') . '/../');
+        $packagePath = realpath(__DIR__ . '/../../');
+
+        preg_match(
+            sprintf(';^%s/(.+)$;', $projectPath),
+            $packagePath,
+            $matches
+        );
+        $relativePackagePath = $matches[1];
+
+        self::ensureFile($packagePath . '/cypress.json', $projectPath . '/cypress.json', $io);
+        self::ensureLink($relativePackagePath . '/cypress', $projectPath . '/cypress', $io);
     }
 
-    public function deactivate(Composer $composer, IOInterface $io)
+    public function deactivate(Composer $composer, IOInterface $io): void
     {
-        // TODO: Implement deactivate() method.
+        // no-op
     }
 
-    public function uninstall(Composer $composer, IOInterface $io)
+    public function uninstall(Composer $composer, IOInterface $io): void
     {
-        // TODO: Implement uninstall() method.
+        // no-op
+    }
+
+    private static function ensureFile(string $source, string $target, IOInterface $io) : void
+    {
+        if (file_exists($target)) {
+            return;
+        }
+        $io->write(sprintf('Copying %s to %s', $source, $target));
+        copy($source, $target);
+    }
+
+    private static function ensureLink(string $source, string $target, IOInterface $io) : void
+    {
+        if (file_exists($target)) {
+            return;
+        }
+        $io->write(sprintf('Linking %s to %s', $target, $source));
+        symlink($source, $target);
     }
 }
